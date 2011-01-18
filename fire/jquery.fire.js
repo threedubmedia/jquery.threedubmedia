6bee54367f48d5bd0c1603e1a5869ec00844c491
Michael Helgeson <mike@bamboo-entertainment.com>
2011-01-18 10:39:38 -0500

    rewrite for easier usage and better compatability with custom events.

--- fire/jquery.fire.js
+++ fire/jquery.fire.js
@@ -12,6 +12,12 @@ $.fire = function( element, type, opts ){
 	this.element = element;
 	this.type = type;
 	this.opts = $.extend( {}, $.fire.defaults, opts );
+	// translate pageX to clientX
+	if ( this.opts.pageX && !this.opts.clientX )	
+		this.opts.clientX = this.opts.pageX - $( window ).scrollLeft();
+	// translate pageY to clientY
+	if ( this.opts.pageY && !this.opts.clientY )	
+		this.opts.clientY = this.opts.pageY - $( window ).scrollTop();
 	this.event = this.create( type, opts );
 	this.dispatch();
 };
@@ -45,12 +51,42 @@ $.fire.prototype = {
 			case "mouseout":
 			case "click":
 			case "dblclick":
+			case "touchstart":
+			case "touchmove":
+			case "touchend":
 				return this.mouse();
 			case "keyup":
 			case "keypress":
 			case "keydown":
 				return this.key();
+			default:
+				return this.event();
 		}
+	},
+	event: function(){
+		var event, opts = this.opts;
+		if ( document.createEvent ){
+			event = document.createEvent("HTMLEvents");
+			event.initEvent(
+				this.type, 
+				opts.bubbles, 
+				opts.cancelable
+			);
+			$.extend( event, { 
+				view: opts.view,
+				ctrlKey: opts.ctrlKey, 
+				altKey: opts.altKey, 
+				shiftKey: opts.shiftKey, 
+				metaKey: opts.metaKey,
+				keyCode: opts.keyCode, 
+				charCode: opts.charCode,
+				button: opts.button
+			});
+		} 
+		else if ( document.createEventObject ) {
+			event = $.extend( document.createEventObject(), opts );
+		}
+		return event;			
 	},
 	mouse: function(){
 		var event, opts = this.opts;
@@ -71,11 +107,11 @@ $.fire.prototype = {
 				opts.shiftKey, 
 				opts.metaKey,
 				opts.button, 
-				$( opts.releatedTarget )[0] || document.body.parentNode 
+				$( opts.relatedTarget )[0] || document.body.parentNode 
 			);
 		} 
 		else if ( document.createEventObject ) {
-			event = $.extend( document.createEventObject(), opts );
+			event = this.event();
 			event.button = { 0:1, 1:4, 2:2 }[ event.button ] || event.button;
 		}
 		return event;
@@ -99,25 +135,11 @@ $.fire.prototype = {
 				);
 			} 
 			catch ( err ){
-				event = document.createEvent("Events");
-				event.initEvent(
-					this.type, 
-					opts.bubbles, 
-					opts.cancelable
-				);
-				$.extend( event, { 
-					view: opts.view,
-					ctrlKey: opts.ctrlKey, 
-					altKey: opts.altKey, 
-					shiftKey: opts.shiftKey, 
-					metaKey: opts.metaKey,
-					keyCode: opts.keyCode, 
-					charCode: opts.charCode
-				});
+				event = this.event();
 			}
 		} 
 		else if ( document.createEventObject ){
-			event = $.extend( document.createEventObject(), opts );
+			event = this.event();
 		}
 		if ( $.browser.msie || $.browser.opera ){
 			event.keyCode = opts.charCode > 0 ? opts.charCode : opts.keyCode;
